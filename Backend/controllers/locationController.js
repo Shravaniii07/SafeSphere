9a0d16a9e0ff2b5647dd1e853666dff14137bbec
Shravaniii07 <shravanibodare@gmail.com>
2026-03-26 22:13:40 +0530

    backend

---
+++ Backend/controllers/locationController.js
@@ -0,0 +1,39 @@
+import Location from "../models/Location.js";
+
+export const updateLocation = async (req, res) => {
+  try {
+    const { latitude, longitude } = req.body;
+
+    const location = await Location.create({
+      // userId: req.user._id,
+      userId: "dummyUser123",
+      latitude,
+      longitude
+    });
+
+    res.status(201).json({
+      success: true,
+      location
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getLocation = async (req, res) => {
+  try {
+    const location = await Location.findOne({
+      userId: req.params.userId
+    }).sort({ timestamp: -1 });
+
+    res.json({
+      success: true,
+      location
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
