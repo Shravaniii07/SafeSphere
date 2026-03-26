008b55e035e6f4b8ca4fbaeb1b497019299da70f
Samruddhi3802 <sahindrakarsamruddhi01@gmail.com>
2026-03-26 21:11:19 +0530

    Travel Safety (Trip) feature added without testing

--- Backend/routes/tripRoutes.js
+++ Backend/routes/tripRoutes.js
@@ -10,13 +10,10 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// router.post("/start", protect, startTrip);
-// router.post("/update-location", protect, updateLocation);
-// router.get("/recent", protect, getRecentTrips);
+router.post("/start", protect, startTrip);
+router.post("/update-location", protect, updateLocation);
+router.get("/recent", protect, getRecentTrips);
 
-router.post("/start", startTrip);
-router.post("/update-location", updateLocation);
-router.get("/recent", getRecentTrips);
 
 // public
 router.get("/track/:trackingId", trackTrip);
